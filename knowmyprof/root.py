f3f6077693431026e215ff92fbbc001a0d85f532
Andy Haden <andyh2@me.com>
2016-11-12 22:40:59 -0800

    up

--- knowmyprof/root.py
+++ knowmyprof/root.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from . import professor
+from . import academic_knowledge
 import inflect
 
 app = Flask(__name__)
@@ -12,18 +12,18 @@ def home():
 def university(name):
     name = name.replace('-', ' ')
     return render_template('university.html',
-        histogram=professor.histogram_for_uni(name),
+        histogram=academic_knowledge.histogram_for_uni(name),
         university=name.title())
 
 UNIVERSITIES = ['university of california davis', 'university of california berkeley']
 @app.route('/search')
 def search():
   q = request.args.get('q').lower()
-  results_by_uni = professor.search_instructor(q, UNIVERSITIES)
-  histogram_by_university = professor.instructor_histogram_by_university(results_by_uni)
-  overall_histogram = professor.get_overall_histogram(histogram_by_university)
-  year_count = professor.year_count(overall_histogram)
-  publication_count = professor.publication_count(overall_histogram)
+  results_by_uni = academic_knowledge.search_instructor(q, UNIVERSITIES)
+  histogram_by_university = academic_knowledge.instructor_histogram_by_university(results_by_uni)
+  overall_histogram = academic_knowledge.get_overall_histogram(histogram_by_university)
+  year_count = academic_knowledge.year_count(overall_histogram)
+  publication_count = academic_knowledge.publication_count(overall_histogram)
   return render_template('search.html',
     query=q,
     query_inflected=inflect.engine().plural(q),
